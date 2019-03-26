0f0960d21b7186647e506a33de67a21ace83993a
AleeWeb <aalexanderpr@gmail.com>
2019-03-26 01:19:28 -0400

    Update maps service.ts for the data object to show up in the console first.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MapsService } from './maps.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ang-google-maps';
+  
+  lat: string = '';
+  lng: string = '';
+  city: string = '';
+
+  location: Object;
+
+  constructor(private map: MapsService){}
+
+  ngOnInit() {
+    this.map.getLocation().subscribe(data => {
+      console.log(data);
+      this.lat = data.latitude;
+      this.lng = data.longitude;
+      this.city = data.city;
+    })
+  }
 }
--- src/app/maps.service.ts
+++ src/app/maps.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+interface Location {
+  latitude: string;
+  longitude: string;
+  city: string;
+}
+
 
 @Injectable({
   providedIn: 'root'
 })
-export class MapsService {
 
-  constructor() { }
+export class MapsService {
+  constructor(private http: HttpClient) { }
+
+  getLocation() {
+    return this.http.get<Location>('http://api.ipapi.com/api/check?access_key=[Your Own ipapi Key HERE]')
+  }
 }
